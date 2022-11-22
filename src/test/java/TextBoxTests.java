f1ba6f93010e24ab71570976e63f3bb5308e2cbd
Alexsey Fadeev <fadeevlesha@list.ru>
2022-11-23 00:49:06 +0300

    added method TextBoxTests

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,8 +1,12 @@
 import com.codeborne.selenide.Selenide;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class TextBoxTests {
 
     void fillFormTest(){
-        Selenide.open("https://demoqa.com/text-box");
+        open("https://demoqa.com/text-box");
+        $("[userName]").setValue("Egor");
     }
 }
