12e4fef8efa607d7b70116892324feeb4c79be74
Alexsey Fadeev <fadeevlesha@list.ru>
2022-11-24 11:46:26 +0300

    added finished the test

--- src/test/java/tests/RegistrTests.java
+++ src/test/java/tests/RegistrTests.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -13,8 +14,9 @@ public class RegistrTests {
     @BeforeAll
     static void beforeAll(){
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+
 
     }
     @Test
@@ -24,7 +26,7 @@ public class RegistrTests {
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('#footer').remove()");
+        executeJavaScript("$('footer').remove()");
 
         $("#firstName").setValue(userName);
         $("#lastName").setValue("Egorich");
@@ -49,7 +51,11 @@ public class RegistrTests {
         $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
-        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#stateCity-wrapper").$(byText("Gurgaon")).click();
         $("#submit").click();
+
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(userName), text("Egorich"), text("Egor@egor.com"), text("8926012345"));
     }
 }
